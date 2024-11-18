ff7ef5ff8bcb6ed26fa9593449aee415257fa08a
Artur Pacut <arturpacut904@gmail.com>
2024-11-18 21:46:43 +0000

    Hidden nav bar fade in
    Started work on fade out

--- templates/src/script.js
+++ templates/src/script.js
@@ -10,10 +10,34 @@ function lightMode() {
     document.getElementById("lightModeButton").style.display = "block";
     document.getElementById("darkModeButton").style.display = "none";
 }
-function openNav() {
-    document.getElementById("hiddenNav").style.display = "block";
+function openNav(){
+    var hiddenNav = document.getElementById("hiddenNav");
+    hiddenNav.style.display = "block";
+    hiddenNav.style.opacity = "0";
+    var fade = setInterval(function () {
+        if (hiddenNav.style.opacity < "1") {
+            hiddenNav.style.opacity = parseFloat(hiddenNav.style.opacity) + 0.1;
+        } else {
+            clearInterval(fade);
+        }
+    }, 16);
     document.getElementById("navButton").style.display = "none";
 }
+
+// function closeNav() {
+//     var hiddenNav = document.getElementById("hiddenNav");
+//     hiddenNav.style.display = "none";
+//     hiddenNav.style.opacity = "1";
+//     setInterval = 16;
+//     var fade = setInterval(function () {
+//         if (hiddenNav.style.opacity > "0") {
+//             hiddenNav.style.opacity = parseFloat(hiddenNav.style.opacity) - 0.1;
+//         } else {
+//             clearInterval(fade);
+//         }
+//     }, 16);
+//     document.getElementById("navButton").style.display = "block";
+// }
 function closeNav() {
     document.getElementById("hiddenNav").style.display = "none";
     document.getElementById("navButton").style.display = "block";
